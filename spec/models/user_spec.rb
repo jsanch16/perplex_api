90807bc51bf8fcab71fbc3faf3173dcd68649176
Juan Sanchez <juansanchez3096@sbcglobal.net>
2017-12-02 20:56:40 -0800

    added auth_token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe User, type: :model do
   it { is_expected.to allow_value('example@domain.com').for(:email) }
 
   it { is_expected.to be_valid }
+
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      Devise.stub(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eq("auniquetoken123")
+    end
+
+    it "generates another token when one has already been taken" do
+      existing_user = FactoryBot.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end
 end
