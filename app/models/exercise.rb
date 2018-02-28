292c26ecc734f3f865073e3472be94b21545e82a
juansanchez@coraxcyber.com <juansanchez@CCLT12.local>
2018-02-28 15:43:24 -0800

    fix Exercise bicep queries

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -6,9 +6,9 @@ class Exercise < ApplicationRecord
   has_many :exercises_muscles, dependent: :destroy
   has_many :muscles, through: :exercises_muscles
 
-  scope :all_biceps, -> { joins(:exercises_muscles).where(exercises_muscles: {muscle_id: Muscle.all_biceps.ids}) }
-  scope :inner_biceps, -> { joins(:exercises_muscles => :muscles).where(muscles: {name: 'Inner biceps'})}
-  scope :outer_biceps, -> { joins(:exercises_muscles => :muscles).where(muscles: {name: 'Outer biceps'})}
-  scope :overall_biceps, -> { joins(:exercises_muscles => :muscles).where(muscles: {name: 'Overall biceps'})}
+  scope :all_biceps, -> { joins(:muscles).where(muscles: {id: Muscle.all_biceps.ids}) }
+  scope :inner_biceps, -> { joins(:muscles).where(muscles: {name: 'Inner biceps'})}
+  scope :outer_biceps, -> { joins(:muscles).where(muscles: {name: 'Outer biceps'})}
+  scope :overall_biceps, -> { joins(:muscles).where(muscles: {name: 'Overall biceps'})}
 
 end
